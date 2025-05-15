a0bfdd7dd4a3d6cfb5caf1d1ce7136a96e67bf50
zahri04 <zhry03@gmail.com>
2025-05-15 13:37:05 +0100

    Update Axios.js

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance=axios.create({
-    baseURL: "http://localhost:8080/api",
+    baseURL: "http://backend:8080/api",
     withCredentials: true,
 });
 
