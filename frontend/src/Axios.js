e7a317b7946ada3e0b9cab7bd6cb389a0bf25367
amine <zhry04@gmail.com>
2025-05-16 14:49:52 +0100

    LATEST COMMIT BY ZAHRI04

--- frontend/src/Axios.js
+++ frontend/src/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance=axios.create({
-    baseURL: "http://backend:8080/api",
+    baseURL: process.env.REACT_APP_API_URL,
     withCredentials: true,
 });
 
