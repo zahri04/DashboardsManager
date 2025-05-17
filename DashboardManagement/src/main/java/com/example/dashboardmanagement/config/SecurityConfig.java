2ae0e6ef7a0fb328988219d221f0bea2794d353b
amine <zhry04@gmail.com>
2025-05-17 22:13:21 +0100

    Commiting changes for opening Cors

--- DashboardManagement/src/main/java/com/example/dashboardmanagement/config/SecurityConfig.java
+++ DashboardManagement/src/main/java/com/example/dashboardmanagement/config/SecurityConfig.java
@@ -65,7 +65,7 @@ public class SecurityConfig  {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:3000"));
+        configuration.setAllowedOrigins(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
