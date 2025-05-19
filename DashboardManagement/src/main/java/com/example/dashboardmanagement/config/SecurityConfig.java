811416691abbd6dffeef550ba5d5892ae839d92f
zahri04 <zhry03@gmail.com>
2025-05-19 10:19:18 +0100

    Update SecurityConfig.java

--- DashboardManagement/src/main/java/com/example/dashboardmanagement/config/SecurityConfig.java
+++ DashboardManagement/src/main/java/com/example/dashboardmanagement/config/SecurityConfig.java
@@ -69,7 +69,7 @@ public class SecurityConfig  {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:80"));
+        configuration.setAllowedOriginPatterns(List.of("*"));
         configuration.setAllowedMethods(List.of("*"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
